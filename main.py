18ddfd190a90615bc733779418e08544b4242266
choi-p-git <choi.p.git@gmail.com>
2025-03-14 19:53:23 -0500

    updated main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("greetings boots")
+frankenstein = "./books/frankenstein.txt"
+words = ""
+
+def get_book_text(book_path):
+    with open(book_path) as f:
+        book_text = f.read()
+        return book_text
+    
+def main(book):
+    book_text = get_book_text(book)
+    words = book_text.split()
+    print(f"{len(words)} words found in the document")
+
+main(frankenstein)
