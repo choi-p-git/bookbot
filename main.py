04d92808fcf45a4dd42408f939a1d3aa25f66ee2
choi-p-git <choi.p.git@gmail.com>
2025-03-16 12:08:45 -0500

    added moby dick and pride and prejudice. updated main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-frankenstein = "books/frankenstein.txt"   # set book location
+import sys
+
+if len(sys.argv) < 2:
+    print ("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 
 # Word_counter function in remote location
 
@@ -6,8 +10,8 @@ from stats import word_counter
 from stats import char_sort
 
 print("============ BOOKBOT ============")
-print(f"Analyzing book found at {frankenstein}...")
+print(f"Analyzing book found at {sys.argv[1]}...")
 print("----------- Word Count ----------")
-word_counter(frankenstein)
+word_counter(sys.argv[1])
 print("--------- Character Count -------")
-char_sort(frankenstein)
+char_sort(sys.argv[1])
