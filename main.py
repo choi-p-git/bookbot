12ff783a766579087aeaa99441c1de84aa61f557
choi-p-git <choi.p.git@gmail.com>
2025-03-15 23:24:03 -0500

    added character sorting function, updated main bookbot

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-frankenstein = "./books/frankenstein.txt"   # set book location
+frankenstein = "books/frankenstein.txt"   # set book location
 
 # Word_counter function in remote location
 
 from stats import word_counter
-from stats import char_counter
+from stats import char_sort
 
+print("============ BOOKBOT ============")
+print(f"Analyzing book found at {frankenstein}...")
+print("----------- Word Count ----------")
 word_counter(frankenstein)
-char_counter(frankenstein)
+print("--------- Character Count -------")
+char_sort(frankenstein)
--- stats.py
+++ stats.py
@@ -1,30 +1,39 @@
 words = ""  # Set words as empty string
 char_list = {"a" : 0, "b" : 0, "c" : 0, "d" : 0, "e" : 0, "f" : 0, "g" : 0, "h" : 0, "i" : 0, "j" : 0, "k" :0, "l" : 0, "m" :0, "n" : 0, "p" : 0, "q" : 0, "r" : 0, "s" : 0,
               "t" : 0, "u" : 0, "v" : 0, "w" : 0, "x" : 0, "y" : 0, "z" : 0}
-
+dict_list = []
 # Word Counter Function
 
 def word_counter(book):
     book_text = get_book_text(book)  # Retrieving text from get_book_text function
     words = book_text.split()        # Converting text string to word strings
-    print(f"{len(words)} words found in the document")
+    print(f"Found {len(words)} total words")
 
 # Character Counter Function
 
 def char_counter(book):
-    book_text = get_book_text(book)  # Retrieving text from get_book_text function
-    characters = book_text.lower()
-    for c in characters:
-        if c in char_list:
-            char_list[c] = char_list[c]+1
+    book_text = get_book_text(book)         # Retrieving text from get_book_text function
+    characters = book_text.lower()          # Converting text to lowercase
+    for c in characters:                    # Iterating character in string
+        if c in char_list:                  # Determining if character exist in dictionary
+            char_list[c] = char_list[c]+1   # Add 1 to existing count
         else:
-            char_list[c] = 1
-    print(char_list)
+            char_list[c] = 1                # Character not in dictionary, set character as key and value to 1
+    return char_list                        # Return dictionary
 
-# Characters to Dictionary Function
+# Character Dictionary Sorting Function
 
-
-
+def char_sort(book):
+    unsorted_list = char_counter(book)                      # Acquire character dictionary from Character Counter Function
+    for char, count in unsorted_list.items():               # Iterating key:value pair to create new list of 2 key dictionary
+        temp_dict = {"character" : char, "count" : count}   # Setting "String" key to str / int values
+        dict_list.append (temp_dict)                        # Appending each temp dictionary to list
+    dict_list.sort(reverse=True, key=lambda x: x["count"])  # Sorting list by x: x["count"] through special key=lambda function in reverse
+    for item in dict_list:                                  # Iterating each dictionary
+        if item["character"].isalpha():                     # Determin if key "character" for dictionary is alphabetical character
+            print (f"{item["character"]}: {item["count"]}") # Print as f-string
+        else:
+            continue                                        # Skip function
 
 
 # Book Retrieval Function
